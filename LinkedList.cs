1a72b461c712cfca2a055158b19ba806ed6f3071
Aakankshap16 <aakankshap971@gmail.com>
2023-03-28 13:35:23 +0530

    add insertion method uc4

--- LinkedList.cs
+++ LinkedList.cs
@@ -6,74 +6,65 @@
 
         public void Add(int Data)
         {
-            Node node = new Node(Data);
-            if (this.head == null)
-            {
-                this.head = node;
-            }
-            else
-            {
-                Node temp = head;
-                while (temp.Next != null)
-                {
-                    temp = temp.Next;
-                }
-                temp.Next = node;
-
-            }
-            Console.WriteLine($"{node.Data} inserted into linked list ");
+          Node node = new Node(Data);
+        if (this.head == null)
+        {
+            this.head = node;
         }
-
-        public void Display()
+        else
         {
             Node temp = head;
-            if (temp == null)
+            while (temp.Next != null)
             {
-                Console.WriteLine("Linkedlist is empty");
-                return;
-            }
-            while (temp != null)
-            {
-                Console.Write(temp.Data + " ");
                 temp = temp.Next;
             }
+            temp.Next = node;
         }
+        Console.WriteLine("{0} inserted into Linked List", node.Data);
+    }
 
-        public Node Insertion(int Position, int Data)
+    public void Display()
+    {
+        Node temp = this.head;
+        if (temp == null)
         {
-            if (Position < 1)
-            {
-                Console.WriteLine(" Not Possible");
-            }
-            else if (Position == 1)
-            {
-                var newNode = new Node(Data);
-                newNode.Next = this.head;
-                head = newNode;
-            }
-            else
-            {
-                int currentPosition = 1;
-                Node temp = head;
-                while (temp != null && currentPosition < Position - 1)
-                {
-                    temp = temp.Next;
-                    currentPosition++;
-                }
-                if (temp == null)
-                {
-                    Console.WriteLine("Position out of the range.");
-                }
-                else
-                {
-                    Node newNode = new Node(Data);
-                    newNode.Next = temp.Next;
-                    temp.Next = newNode;
-                    Console.WriteLine($"{newNode.Data} inserted at position {Position}");
-                }
-            }
-            return head;
+            Console.WriteLine("Linked List is empty");
+            return;
+        }
+        while (temp != null)
+        {
+            Console.WriteLine(temp.Data + " ");
+            temp = temp.Next;
         }
     }
+    internal Node Insertion(int position, int Data)
+    {
+        if (position < 1)
+            Console.WriteLine("Invalid position");
+        if (position == 1)
+        {
+            var newNode = new Node(Data);
+            newNode.Next = this.head;
+            head = newNode;
+        }
+        else
+        {
+            while (position-- != 0)
+            {
+                if (position == 1)
+                {
+                    Node node = new Node(Data);
+                    node.Next = this.head.Next;
+                    head.Next = node;
+                    break;
+                }
+                head = head.Next;
+            }
+            if (position != 1)
+                Console.WriteLine("Position out of range");
+        }
+        return head;
+    }
+}
 
 }
