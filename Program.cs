c518e8ffb737c60d02c00e17a369e2098b9d8bed
Aakankshap16 <aakankshap971@gmail.com>
2023-03-28 15:43:51 +0530

    call delete last method uc6

--- Program.cs
+++ Program.cs
@@ -2,30 +2,30 @@
 {
     class Program
     {
-       
-            public static void Main(string[] args)
-            {
-                Console.WriteLine("Welcome to Linkedlist problem!");
-                Console.WriteLine("**********************************");
-                LinkedList list = new LinkedList();
-                list.Add(56);
-                Console.WriteLine("Node with data 56 is First Created");
-                 Console.WriteLine(" ");
-                 list.Add(30);
-                Console.WriteLine("Next Append 70 to 56");
-                Console.WriteLine(" ");
-                list.Add(70);
-                Console.WriteLine("Finally Append 70 to 30");
-                Console.WriteLine(" ");
-                Console.WriteLine("LinkedList Sequence:");
-                list.Display();
-                 
-                list.Delete();
-            Console.WriteLine("After delete first element ");
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linkedlist problem!");
+            Console.WriteLine("**********************************");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            Console.WriteLine("Node with data 56 is First Created");
+            Console.WriteLine(" ");
+            list.Add(30);
+            Console.WriteLine("Next Append 70 to 56");
+            Console.WriteLine(" ");
+            list.Add(70);
+            Console.WriteLine("Finally Append 70 to 30");
+            Console.WriteLine(" ");
+            Console.WriteLine("LinkedList Sequence:");
+            list.Display();
+
+            list.DeleteLast();
+            Console.WriteLine("After delete Last element ");
             Console.WriteLine("LinkedList Sequence:");
             list.Display();
 
         }
-        
+
     }
 }
